9e617262018fdb583481e0c40f799d5aad8c1583
Mac <Mac@macui-imac-6.local>
2015-10-17 14:46:30 +0900

    Draft

---
+++ Practice04-Unwind/SecondVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondVC.swift
+//  Practice04-Unwind
+//
+//  Created by Mac on 2015. 10. 17..
+//  Copyright © 2015년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC : UIViewController {
+	
+	@IBAction func unwindToSecond(segue: UIStoryboardSegue) {
+		// none
+	}
+	
+}
---
+++ Practice04-Unwind/ThirdVC.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdVC.swift
+//  Practice04-Unwind
+//
+//  Created by Mac on 2015. 10. 17..
+//  Copyright © 2015년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ThirdVC : UIViewController {
+	
+	@IBAction func unwindToThird(segue: UIStoryboardSegue) {
+		// none
+	}
+	
+	
+}
